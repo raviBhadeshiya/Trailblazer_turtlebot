c3ff301685723915b2e2902d0312319f5bd720df
Ravi Bhadeshiya <ravib@terpmail.umd.edu>
2017-04-17 20:43:47 -0400

    line match complete

--- adventure_slam/adventure_slam.py
+++ adventure_slam/adventure_slam.py
@@ -5,10 +5,15 @@ from geometry_msgs.msg import Point
 from visualization_msgs.msg import MarkerArray, Marker
 from nav_msgs.msg import OccupancyGrid, Odometry
 
+import random as random
+
 import cv2, math, pcl
 import numpy as np
 
 pub = rospy.Publisher('/slam_debug', MarkerArray)
+MIN_Inliers=75
+
+var_pLines=[]
 
 
 def get_line(p1, v1, id_, color=(0,0,1)):
@@ -48,10 +53,82 @@ def removeInlinear(pcl_points,data):
 	return pcl_points_set
 
 
+def fitLine(p):
+	## create a segmenter object
+	seg = p.make_segmenter()
+	seg.set_model_type(pcl.SACMODEL_LINE)
+	seg.set_method_type(pcl.SAC_RANSAC)
+	seg.set_distance_threshold (0.003)
+	## apply RANSAC
+	indices, model = seg.segment()
+	return indices,model
+
+
+def angleBetweenLines(line1,line2):
+	(x1,y1),(x2,y2)=line1
+	(px1,py1),(px2,py2)=line2
+
+	angel=abs(math.atan((y2-y1)/float(x2-x1))-math.atan((py2-py1)/float(px2-px1)))
+	# angel=abs(math.atan2((y2-y1),(x2-x1))-math.atan2((py2-py1),(px2-px1)))
+	return angel
+
+def isParallel(line1,line2,thre=5.0):
+
+	angle=angleBetweenLines(line1,line2)
+	
+	if angle <= thre * (math.pi/180):
+			return True
+
+	return False
+
+
+def parallelDist(line1,line2):
+	(x1,y1),(x2,y2)=line1
+	(px1,py1),(px2,py2)=line2
+	
+	slope=((y2-y1)/float(x2-x1)) + ((py2-py1)/float(px2-px1))
+	slope/=2.0
+
+
+	c=y1-slope*x1
+	pc=py1-slope*px1
+
+	return abs(c-pc)/math.hypot(slope,1)
+
+
+def lineMath(line,varPLines):
+	result=None
+	minimum=float('Inf')
+	for index in varPLines:
+		if isParallel(line,index):
+			dist=parallelDist(line,index)
+			if dist < minimum:
+				minimum=dist
+				result=index
+	return result
+
+def computeOdom(lines,varPlines):
+
+	t,r=0.0,0.0
+
+	translation=[]
+	rotation=[]
+
+	for index in lines:
+		result=lineMath(index,varPlines)
+		if result != None:
+			translation.append(parallelDist(index,result))
+			rotation.append(angleBetweenLines(index,result))
+	
+	if len(translation)!=0 and len(rotation) != 0:
+		t=sum(translation)/len(translation)
+		r=sum(rotation)/len(rotation)
+
+	return t,r
 
 def laser_callback(scan):
 	marker_array = MarkerArray()
-
+	var_lines=[]
 	# Convert the laserscan to coordinates
 	angle = scan.angle_min
 	points = []    
@@ -64,53 +141,51 @@ def laser_callback(scan):
 			continue
 
 		points.append([r * math.sin(theta), r * math.cos(theta)]) 
-
-	# Fit the line
+	
 	## convert points to pcl type
 	points = np.array(points, dtype=np.float32)
 	pcl_points = np.concatenate((points, np.zeros((len(points), 1))), axis=1)    
 	p = pcl.PointCloud(np.array(pcl_points, dtype=np.float32))
 	
-	## create a segmenter object
-	seg = p.make_segmenter()
-	seg.set_model_type(pcl.SACMODEL_LINE)
-	seg.set_method_type(pcl.SAC_RANSAC)
-	seg.set_distance_threshold (0.003)
+	if p.size > MIN_Inliers:
+		#fit the first
+		indices, model = fitLine(p)
+		
+		# print "Found", len(indices), "inliers", model
+		marker_array.markers.append(get_line((model[1], model[0]), (model[4], model[3]), 0))
+		
+		var_lines.append(((model[1], model[0]), (model[4], model[3])))
+
+		pub.publish(marker_array)
+
+		i=1
+		pcl_points_next=removeInlinear(pcl_points,p.extract(indices))
+		
+		while len(pcl_points_next)/3 > MIN_Inliers:
+		#fit next line
+			p = pcl.PointCloud(np.array(pcl_points_next, dtype=np.float32))
+			indices, model = fitLine(p)
+			# print "Found", len(indices), "inliers", model
+
+			marker_array.markers.append(get_line((model[1], model[0]), (model[4], model[3]), i))
+			
+			var_lines.append(((model[1], model[0]), (model[4], model[3])))
+
+			pub.publish(marker_array)
+
+			pcl_points_next=removeInlinear(pcl_points_next,p.extract(indices))
+
+			i+=1
+
+	global var_pLines
 	
-	## apply RANSAC
-	indices, model = seg.segment()
-	print "Found", len(indices), "inliers", model
-	
-	marker_array.markers.append(get_line((model[1], model[0]), (model[4], model[3]), 0))
-	pub.publish(marker_array)
+	if len(var_pLines)!=0:
+		t,r=computeOdom(var_lines,var_pLines)
+		print str(t)+"----------"+str(r*(180/math.pi))
 
+	var_pLines=var_lines
 
-	pcl_points_set=removeInlinear(pcl_points,p.extract(indices))
-
-	
-	p = pcl.PointCloud(np.array(pcl_points_set, dtype=np.float32))
-	
-	## create a segmenter object
-	seg = p.make_segmenter()
-	seg.set_model_type(pcl.SACMODEL_LINE)
-	seg.set_method_type(pcl.SAC_RANSAC)
-	seg.set_distance_threshold (0.003)
-	
-	## apply RANSAC
-	indices, model = seg.segment()
-	print "Found", len(indices), "inliers", model
-	
-	marker_array.markers.append(get_line((model[1], model[0]), (model[4], model[3]), 1,(0,1,0)))
-	pub.publish(marker_array)
-
-
-	# # OpenCV line fitter - least squares
-	# line = cv2.fitLine(points, 2, 0, 0.01, 0.01)
-	# # Publish detected lines so we can see them in Rviz
-	# marker_array.markers.append(get_line((line[3], line[2]), (line[1], line[0]), 1, (0,1,0)))
-	# pub.publish(marker_array)
-
-
+			
 def main():
 	rospy.init_node('adventure_slam', anonymous=True)
 	rospy.Subscriber("/scan", LaserScan, laser_callback)

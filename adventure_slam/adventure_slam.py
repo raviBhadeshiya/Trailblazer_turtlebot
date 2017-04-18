430c2e2101037e87e9a55ecf48a8d396dd83ce63
Ravi Bhadeshiya <ravib@terpmail.umd.edu>
2017-04-18 19:50:19 -0400

    tf and vo implement-corection_needed

--- adventure_slam/adventure_slam.py
+++ adventure_slam/adventure_slam.py
@@ -1,19 +1,61 @@
 #!/usr/bin/env python
 import rospy, pcl_ros, tf
 from sensor_msgs.msg import LaserScan
-from geometry_msgs.msg import Point
+from geometry_msgs.msg import Point, Quaternion
 from visualization_msgs.msg import MarkerArray, Marker
 from nav_msgs.msg import OccupancyGrid, Odometry
-
 import random as random
-
 import cv2, math, pcl
 import numpy as np
 
-pub = rospy.Publisher('/slam_debug', MarkerArray)
-MIN_Inliers=75
 
+#Global Variables
+pub = rospy.Publisher('/slam_debug', MarkerArray)
+pub1=rospy.Publisher('/vo',Odometry,queue_size=10)
+antena= tf.TransformBroadcaster()
+
+MIN_Inliers=75
 var_pLines=[]
+
+
+
+class odomVisual():
+	def __init__(self,pub1,antena,time,x=0.0,y=0.0,theta=0.0):
+		self.x=x
+		self.y=y
+		self.theta=theta
+		self.pub1=pub1
+		self.antena=antena		
+		self.last_time=time
+
+	def odomVisualPublisher(self,v,th,time):
+		self.current_time=time
+
+		dt=(self.current_time.to_sec()-self.last_time.to_sec())
+
+		self.theta=th*dt
+		self.x+=v*math.cos(self.theta)*dt
+		self.y+=v*math.sin(self.theta)*dt
+
+		msg=Odometry()
+		msg.header.stamp=self.current_time
+		msg.pose.pose.position=Point(self.x,self.y,0.0)
+		tx,ty,tz,tw=tf.transformations.quaternion_from_euler(0.0,0.0,self.theta)
+		msg.pose.pose.orientation=Quaternion(tx,ty,tz,tw)		
+
+		self.pub1.publish(msg)
+
+		pos = msg.pose.pose
+		self.antena.sendTransform((pos.position.x, pos.position.y, pos.position.z),
+			(pos.orientation.x,pos.orientation.y,pos.orientation.z,pos.orientation.w),
+			self.current_time,  "/odom_visual","/base_footprint")
+
+		self.last_time = self.current_time
+
+		return self.x,self.y,self.theta
+
+
+
 
 
 def get_line(p1, v1, id_, color=(0,0,1)):
@@ -64,12 +106,15 @@ def fitLine(p):
 	return indices,model
 
 
-def angleBetweenLines(line1,line2):
+def angleBetweenLines(line1,line2,match=True):
 	(x1,y1),(x2,y2)=line1
 	(px1,py1),(px2,py2)=line2
 
-	angel=abs(math.atan((y2-y1)/float(x2-x1))-math.atan((py2-py1)/float(px2-px1)))
-	# angel=abs(math.atan2((y2-y1),(x2-x1))-math.atan2((py2-py1),(px2-px1)))
+	if match:
+		angel=abs(math.atan((y2-y1)/float(x2-x1))-math.atan((py2-py1)/float(px2-px1)))
+	else:
+		angel=(math.atan((y2-y1)/float(x2-x1))-math.atan((py2-py1)/float(px2-px1)))
+
 	return angel
 
 def isParallel(line1,line2,thre=5.0):
@@ -82,19 +127,20 @@ def isParallel(line1,line2,thre=5.0):
 	return False
 
 
-def parallelDist(line1,line2):
+def parallelDist(line1,line2,match=True):
 	(x1,y1),(x2,y2)=line1
 	(px1,py1),(px2,py2)=line2
 	
 	slope=((y2-y1)/float(x2-x1)) + ((py2-py1)/float(px2-px1))
 	slope/=2.0
 
-
 	c=y1-slope*x1
 	pc=py1-slope*px1
 
-	return abs(c-pc)/math.hypot(slope,1)
-
+	if match:
+		return abs(pc-c)/math.hypot(slope,1)
+	else:
+		return (pc-c)/math.hypot(slope,1)
 
 def lineMath(line,varPLines):
 	result=None
@@ -113,12 +159,12 @@ def computeOdom(lines,varPlines):
 
 	translation=[]
 	rotation=[]
-
+	
 	for index in lines:
 		result=lineMath(index,varPlines)
 		if result != None:
-			translation.append(parallelDist(index,result))
-			rotation.append(angleBetweenLines(index,result))
+			translation.append(parallelDist(index,result,match=False))
+			rotation.append(angleBetweenLines(index,result,match=False))
 	
 	if len(translation)!=0 and len(rotation) != 0:
 		t=sum(translation)/len(translation)
@@ -126,7 +172,14 @@ def computeOdom(lines,varPlines):
 
 	return t,r
 
+
+
+voX,voY,voT=0,0,0
+
 def laser_callback(scan):
+	global voX,voY,voTheta
+	odomVisualPub = odomVisual(pub1,antena,rospy.Time.now(),voX,voY,voT)
+	
 	marker_array = MarkerArray()
 	var_lines=[]
 	# Convert the laserscan to coordinates
@@ -164,8 +217,8 @@ def laser_callback(scan):
 		while len(pcl_points_next)/3 > MIN_Inliers:
 		#fit next line
 			p = pcl.PointCloud(np.array(pcl_points_next, dtype=np.float32))
+
 			indices, model = fitLine(p)
-			# print "Found", len(indices), "inliers", model
 
 			marker_array.markers.append(get_line((model[1], model[0]), (model[4], model[3]), i))
 			
@@ -177,11 +230,13 @@ def laser_callback(scan):
 
 			i+=1
 
-	global var_pLines
+	global var_pLines,odomVisualPub,rospy
 	
 	if len(var_pLines)!=0:
 		t,r=computeOdom(var_lines,var_pLines)
-		print str(t)+"----------"+str(r*(180/math.pi))
+		print str(t)+" and "+str(r*(180/math.pi))
+		time=rospy.Time.now()
+		voX,voY,voT=odomVisualPub.odomVisualPublisher(t,r,time)
 
 	var_pLines=var_lines
 

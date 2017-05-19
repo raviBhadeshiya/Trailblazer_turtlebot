c3989576462d9482b71ee414d7ea70a16a6c09da
nr-parikh <nrparikh@umd.edu>
2017-05-19 18:20:29 -0400

    Bugs found

--- adventure_recognition/feature_match.py
+++ adventure_recognition/feature_match.py
@@ -4,11 +4,12 @@ from matplotlib import pyplot as plt
 
 MIN_MATCH_COUNT = 10
 
-img1 = cv2.imread('images/train/bottle_0.jpg',0)          # queryImage
-img2 = cv2.imread('images/train/xtion_cam/frame0004.jpg',0) # trainImage
+img1 = cv2.imread('images/train/temp.jpg',0)          # queryImage
+img2 = cv2.imread('images/train/cpp.jpg',0) # trainImage
 
 # Initiate SIFT detector
 # sift = cv2.SIFT()
+
 sift = cv2.xfeatures2d.SIFT_create()
 
 # find the keypoints and descriptors with SIFT
@@ -40,7 +41,10 @@ if len(good)>MIN_MATCH_COUNT:
     pts = np.float32([ [0,0],[0,h-1],[w-1,h-1],[w-1,0] ]).reshape(-1,1,2)
     dst = cv2.perspectiveTransform(pts,M)
 
+    print dst
     img2 = cv2.polylines(img2,[np.int32(dst)],True,255,3, cv2.LINE_AA)
+    cv2.imshow("img2",img2)
+    # cv2.waitkey(1)
 
 else:
     print "Not enough matches are found - %d/%d" % (len(good),MIN_MATCH_COUNT)
@@ -54,3 +58,56 @@ draw_params = dict(matchColor = (0,255,0), # draw matches in green color
 img3 = cv2.drawMatches(img1,kp1,img2,kp2,good,None,**draw_params)
 
 plt.imshow(img3, 'gray'),plt.show()
+
+
+
+ #  def computeSiftFeatures(self):
+
+ #  	self.capture_image()
+  	
+ #  	print "Captured!"
+ #  	sift = cv2.xfeatures2d.SIFT_create()
+ #  	self.image=cv2.cvtColor(self.image,cv2.COLOR_BGR2GRAY)
+ #  	kp, des = sift.detectAndCompute(self.image,None)
+ #  	flann = cv2.FlannBasedMatcher(self.index_params, self.search_params)
+
+ #  	for i in range(len(self.trainingImages)):
+
+ #  		matches = flann.knnMatch(self.des[i],des,k=2)
+	# 	good = []
+	# 	for m,n in matches:
+	# 	    if m.distance < 0.7*n.distance:
+	# 	        good.append(m)
+
+	# 	if len(good)>self.minMatchCount:
+	# 	    print i
+	# 	    dst_pts = np.float32([ kp[m.queryIdx].pt for m in good ]).reshape(-1,1,2)
+	# 	    src_pts = np.float32([ self.kp[i][m.trainIdx].pt for m in good ]).reshape(-1,1,2)
+
+	# 	    M, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC,5.0)
+	# 	    matchesMask = mask.ravel().tolist()
+
+	# 	    h,w = self.image.shape
+
+	# 	    pts = np.float32([ [0,0],[0,h-1],[w-1,h-1],[w-1,0] ]).reshape(-1,1,2)
+	# 	    dst = cv2.perspectiveTransform(pts,M)
+
+	# 	    img2 = cv2.polylines(self.image,[np.int32(dst)],True,255,3, cv2.LINE_AA)
+	# 	    print "Matched!! - %d/%d" % (len(good),self.minMatchCount)
+	# 	    break
+	# 	else:
+	# 	    print "Not enough matches are found - %d/%d" % (len(good),self.minMatchCount)
+	# 	    matchesMask = None
+
+	# if matchesMask != None:
+	# 	draw_params = dict(matchColor = (0,255,0), 
+	# 		singlePointColor = None,
+	#        matchesMask = matchesMask, flags = 2)
+
+	# 	# img3 = cv2.drawMatches(self.image,kp,img2,self.kp[i],good,None,**draw_params)
+	# 	img3 = cv2.drawMatches(self.trainingImages[i],kp,img2,self.kp[i],good,None,**draw_params)
+	# 	plt.imshow(img3, 'gray'),plt.show()
+
+
+  #-----------------------------------------------------------------------------
+  # Run!
--- adventure_recognition/object_search.py
+++ adventure_recognition/object_search.py
@@ -19,7 +19,8 @@ import copy
 import rospkg
 from trajectory_demo import *
 from actionlib_msgs.msg import *
-
+import os
+from matplotlib import pyplot as plt
 
 #-------------------------------------------------------------------------------
 # Object search class
@@ -41,17 +42,31 @@ class ObjectSearch:
 
 	# Vision
 	self.image = []
-	self.processImage = True #Keep it true to store the  image
+	self.processImage = False #Keep it true to store the  image
 	self.lock = threading.Lock()
 
 	rospack = rospkg.RosPack()
 	self.debugImageDir = rospack.get_path('adventure_recognition') + "/images/debug/"
 	self.trainImageDir = rospack.get_path('adventure_recognition') + "/images/train/"
-	self.trainImageNames = ['bottle_0.jpg', 'bottle_1.jpg', 'bottle_1.jpg']
+	# self.trainImageNames = ['bottle_0.jpg', 'bottle_1.jpg', 'bottle_1.jpg', 'temp.jpg']
+	self.trainImageNames = ['temp.jpg']
 
 	# Initialize node
 	rospy.init_node('adventure_recognition')
 	# rospy.init_node('trajectory_demo')
+
+
+	sift = cv2.xfeatures2d.SIFT_create()
+	
+	self.trainingImages = []
+	# self.kp = []
+	# self.des = []
+
+	for string in self.trainImageNames:
+		img = cv2.imread(os.path.join(self.trainImageDir,string))
+		if img is not None:
+			self.trainingImages.append(img)
+
 
 	# Image subscriber and cv_bridge
 	# self.imageTopic = "/camera/rgb/image_raw/compressed"
@@ -68,7 +83,13 @@ class ObjectSearch:
 	self.move_base.wait_for_server(rospy.Duration(5))
 	rospy.loginfo("Done..")
 
+			# kp, des = sift.detectAndCompute(img,None)
+			# self.kp.append(kp), self.des.append(des)
+			# i+=1
 
+	# self.flannIndexKDtree = 0
+	# self.index_params = dict(algorithm = self.flannIndexKDtree, trees = 5)
+	# self.search_params = dict(checks = 50)
 
   #-------------------------------------------------------------------------------
   # Draw matches between a training image and test image
@@ -86,7 +107,7 @@ class ObjectSearch:
 	rows2 = img2.shape[0]
 	cols2 = img2.shape[1]
 
-	out = np.zeros((max([rows1,rows2]),cols1+cols2,3), dtype='uint8')
+	out = np.zeros((max([rows1,rows2]),cols1+cols2), dtype='uint8')
 
 	# Place the first image to the left
 	out[:rows1,:cols1] = img1
@@ -126,24 +147,35 @@ class ObjectSearch:
   # Image callback
   def imageCallback(self, data):
 
+  	
+
 	# Capture image
 	np_arr = np.fromstring(data.data, np.uint8)
 
-	cv_image = cv2.imdecode(np_arr, cv2.IMREAD_COLOR) # OpenCV 3.0
+	self.cv_image = cv2.imdecode(np_arr, cv2.IMREAD_COLOR) # OpenCV 3.0
 	# cv_image = cv2.imdecode(np_arr, cv2.CV_LOAD_IMAGE_COLOR) # OpenCV 2.4
+
 
 	# Store it if required
 	self.lock.acquire()
+
 	if self.processImage:
-	 print "Capturing image"
-	 self.image = copy.deepcopy(cv_image)
-	 self.processImage = False
+		self.image = copy.deepcopy(self.cv_image)
+	 	self.processImage = False
+
+
+	# for img in self.trainingImages:
+	# 	# print "inside for loop"
+	# 	if self.computeSiftFeatures(img, self.cv_image):
+	# 		print "inside for loop"
+	# 		break
 
 	# Show image
-	cv2.imshow("Image live feed", cv_image)
-	cv2.waitKey(1)
+	# cv2.imshow("Image live feed", self.cv_image)
+	cv2.waitKey(0)
 
 	self.lock.release()
+	# print "Call back out!"
 
   #-------------------------------------------------------------------------------
   # Capture image, display it and save it to the debug folder
@@ -152,22 +184,87 @@ class ObjectSearch:
 	# First get the image to be processed
 	self.processImage = True
 	while (self.processImage):
-	 rospy.sleep(0.01)
+		print "Checking.."
+		rospy.sleep(0.01)
 
-	# Display image
-	self.lock.acquire()
-	cv2.imshow("Captured image", self.image)
-	self.lock.release()
+	#Display image
+	# self.lock.acquire()
+	# cv2.imshow("Captured image", self.image)
+	# self.lock.release()
 
-	# Save image
-	cv2.imwrite(self.debugImageDir+"image_" + str(self.debugImageId) + ".png", self.image)
-	self.debugImageId += 1
+	# # Save image
+	# cv2.imwrite(self.debugImageDir+"image_" + str(self.debugImageId) + ".png", self.image)
+	# self.debugImageId += 1
 
 
-  #-----------------------------------------------------------------------------
-  # Run!
+  def computeSiftFeatures(self, img1, img2):
+  	MIN_MATCH_COUNT = 10
+	# img1 = cv2.imread('images/train/temp.jpg',0)          # queryImage
+	# img2 = cv2.imread('images/train/cpp.jpg',0) # trainImage
+
+	# Initiate SIFT detector
+	# sift = cv2.SIFT()
+
+	#img1=training image
+	#img2=query image
+
+	img1 = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
+	img2 = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
+
+	sift = cv2.xfeatures2d.SIFT_create()
+
+	# find the keypoints and descriptors with SIFT
+	kp1, des1 = sift.detectAndCompute(img1,None)
+	kp2, des2 = sift.detectAndCompute(img2,None)
+
+	FLANN_INDEX_KDTREE = 0
+	index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 5)
+	search_params = dict(checks = 50)
+
+	flann = cv2.FlannBasedMatcher(index_params, search_params)
+
+	matches = flann.knnMatch(des1,des2,k=2)
+
+	# store all the good matches as per Lowe's ratio test.
+	good = []
+	for m,n in matches:
+	    if m.distance < 0.7*n.distance:
+	        good.append(m)
+
+	if len(good)>MIN_MATCH_COUNT:
+	    src_pts = np.float32([ kp1[m.queryIdx].pt for m in good ]).reshape(-1,1,2)
+	    dst_pts = np.float32([ kp2[m.trainIdx].pt for m in good ]).reshape(-1,1,2)
+
+	    M, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC,5.0)
+	    matchesMask = mask.ravel().tolist()
+
+	    h,w = img1.shape
+	    pts = np.float32([ [0,0],[0,h-1],[w-1,h-1],[w-1,0] ]).reshape(-1,1,2)
+	    dst = cv2.perspectiveTransform(pts,M)
+
+	    self.cv_image=cv2.polylines(self.cv_image,[np.int32(dst)],True,255,3, cv2.LINE_AA)
+	    # cv2.imshow("img2",img2)
+	else:
+	    print "Not enough matches are found - %d/%d" % (len(good),MIN_MATCH_COUNT)
+	    matchesMask = None
+	    self.cv_image=img2
+	    return False
+
+	# if matchesMask != None:    
+	# 	draw_params = dict(matchColor = (0,255,0), # draw matches in green color
+	# 	                   singlePointColor = None,
+	# 	                   matchesMask = matchesMask, # draw only inliers
+	# 	                   flags = 2)
+
+		# self.cv_image = cv2.drawMatches(img1,kp1,img2,kp2,good,None,**draw_params)
+		# self.drawMatches(img1, kp1, img2, kp2, good)
+
+
+		# plt.imshow(img3, 'gray'),plt.show()
+	return True
+
+
   def robotMoveToGoal(self,waypoint):
-	#target = Pose(point(),quaternion)
 
 	goal = MoveBaseGoal()
 	goal.target_pose.header.frame_id = 'map'
@@ -177,7 +274,7 @@ class ObjectSearch:
 	self.move_base.send_goal(goal)
 	timeOut = self.move_base.wait_for_result(rospy.Duration(60))
 	state = self.move_base.get_state()
-	print state
+	# print state
 
 	if timeOut and state == GoalStatus.SUCCEEDED:
 		return True
@@ -193,6 +290,17 @@ class ObjectSearch:
 		self.robotMoveToGoal(p)
 		traj.armMoveToGoal()
 		
+		while True:
+			self.capture_image()
+			
+			rospy.sleep(0.1)
+
+			for img in self.trainingImages:
+				if self.computeSiftFeatures(img, self.cv_image):
+					print "inside for loop"
+					break	
+			cv2.imshow("Image live feed", self.cv_image)
+			cv2.waitKey(3)
 	# while True:
 	# 	rospy.sleep(0.1)
 
